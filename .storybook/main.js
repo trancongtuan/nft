4fbf742b814c2a30cbe08d2246e74f248785f4f9
BaoNam99 <obamavn99@gmail.com>
2021-04-26 21:03:58 +0700

    Fix conflict

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,16 @@
+const path = require('path')
+
+const resolve = (p) => path.join(process.cwd(), p)
+
 module.exports = {
+    webpackFinal(config) {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            // https://github.com/system-ui/theme-ui/issues/354#issuecomment-805161307
+            '@emotion/styled': resolve('node_modules/@emotion/styled'),
+        }
+        return config
+    },
     stories: [
         '../stories/**/*.stories.mdx',
         '../stories/**/*.stories.@(js|jsx|ts|tsx)',
