ad030b6cdda8cbec1b419f88bb0785435f5bd139
BaoNam99 <obamavn99@gmail.com>
2021-04-26 21:03:10 +0700

    Fix conflict

--- styles/theme.ts
+++ styles/theme.ts
@@ -45,6 +45,7 @@ export default {
         small: '0 0 4px rgba(0, 0, 0, 0.125)',
         large: '0 0 24px rgba(0, 0, 0, 0.125)',
     },
+    variants: {},
     text: {
         heading: {
             fontFamily: 'sans-serif',
