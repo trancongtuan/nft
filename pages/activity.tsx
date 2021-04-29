09f14909be38136efd2ea16348d3f536e3b75000
Đức Lê <letrongduc1999@gmail.com>
2021-04-29 14:11:44 +0700

    duc/my-items

--- pages/activity.tsx
+++ pages/activity.tsx
@@ -4,9 +4,22 @@ import NavigationBar from '../components/NavigationBar'
 import ActivityCard from '../components/ActivityCard'
 import FilterButton from '../components/FilterButton'
 import Popup from '../components/Popup'
+import Selection from '../components/Selection'
 
 const Activity: FC = () => {
     const [openPopup, setOpenPopup] = useState(false)
+    const [resetFilter, setResetFilter] = useState(false)
+    const [showReset, setShowReset] = useState(false)
+
+    const toogleResetFilter = (): void => {
+        setResetFilter(true)
+        setShowReset(false)
+    }
+
+    const toggleShowReset = (): void => {
+        setShowReset(true)
+        setResetFilter(false)
+    }
 
     return (
         <Box>
@@ -21,15 +34,14 @@ const Activity: FC = () => {
                     Activity
                 </Heading>
                 <Box sx={{ marginTop: '16px' }}>
-                    <Text
-                        sx={{
-                            fontWeight: '900',
-                            borderBottom: '2px solid black',
-                            cursor: 'pointer',
-                        }}
-                    >
-                        All
-                    </Text>
+                    <Selection
+                        borderBottom
+                        items={[
+                            { id: '1', label: 'All' },
+                            { id: '2', label: 'Following' },
+                            { id: '3', label: 'My Activity' },
+                        ]}
+                    />
                 </Box>
                 <Flex sx={{ marginTop: '17px' }}>
                     <Box
@@ -248,22 +260,68 @@ const Activity: FC = () => {
                             },
                         }}
                     >
-                        <Text sx={{ fontWeight: '900' }}>Filters</Text>
+                        <Flex>
+                            <Text sx={{ fontWeight: '900' }}>Filters</Text>
+                            {showReset && (
+                                <Text
+                                    ml={20}
+                                    onClick={() => toogleResetFilter()}
+                                    sx={{
+                                        fontWeight: '900',
+                                        cursor: 'pointer',
+                                        color: 'primary',
+                                    }}
+                                >
+                                    Reset Filters
+                                </Text>
+                            )}
+                        </Flex>
                         <Flex sx={{ marginTop: '16px', flexWrap: 'wrap' }}>
-                            <FilterButton content="Listings" type="listings" />
                             <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                reset={resetFilter}
+                                content="Listings"
+                                type="listings"
+                            />
+                            <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                reset={resetFilter}
                                 content="Purchases"
                                 type="purchases"
                             />
-                            <FilterButton content="Sales" type="sales" />
                             <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                content="Sales"
+                                type="sales"
+                                reset={resetFilter}
+                            />
+                            <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
                                 content="Transfers"
                                 type="transfers"
+                                reset={resetFilter}
                             />
-                            <FilterButton content="Burns" type="burns" />
-                            <FilterButton content="Bids" type="bids" />
-                            <FilterButton content="Likes" type="likes" />
                             <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                reset={resetFilter}
+                                content="Burns"
+                                type="burns"
+                            />
+                            <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                reset={resetFilter}
+                                content="Bids"
+                                type="bids"
+                            />
+                            <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                reset={resetFilter}
+                                content="Likes"
+                                type="likes"
+                            />
+                            <FilterButton
+                                toggleShowReset={() => toggleShowReset()}
+                                reset={resetFilter}
                                 content="Followings"
                                 type="followings"
                             />
@@ -305,14 +363,54 @@ const Activity: FC = () => {
                 label="Filters"
             >
                 <Flex sx={{ width: 400, marginTop: '16px', flexWrap: 'wrap' }}>
-                    <FilterButton content="Listings" type="listings" />
-                    <FilterButton content="Purchases" type="purchases" />
-                    <FilterButton content="Sales" type="sales" />
-                    <FilterButton content="Transfers" type="transfers" />
-                    <FilterButton content="Burns" type="burns" />
-                    <FilterButton content="Bids" type="bids" />
-                    <FilterButton content="Likes" type="likes" />
-                    <FilterButton content="Followings" type="followings" />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Listings"
+                        type="listings"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Purchases"
+                        type="purchases"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Sales"
+                        type="sales"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Transfers"
+                        type="transfers"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Burns"
+                        type="burns"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Bids"
+                        type="bids"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Likes"
+                        type="likes"
+                    />
+                    <FilterButton
+                        toggleShowReset={() => toggleShowReset()}
+                        reset={false}
+                        content="Followings"
+                        type="followings"
+                    />
                     <Button
                         variant="primary"
                         onClick={() => {
