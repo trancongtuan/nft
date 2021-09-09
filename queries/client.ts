64937e4613c9b924d3535e0f53f703be30a32d3b
Stanley <fung1020hk@gmail.com>
2021-09-09 20:42:11 +0800

    fix: wrong api const

--- queries/client.ts
+++ queries/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_ALCHEMY_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
